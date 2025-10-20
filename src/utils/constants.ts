03a69d858915e9638a5819c20af1b536adea9f91
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2025-10-21 01:40:35 +0800

    init: create constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,13 @@
+// Roles
+export const ADMIN_ROLE = "admin";
+export const MANAGER_ROLE = "manager";
+export const DIGITAL_CONTENT_PRODUCER_ROLE = "digital-content-producer";
+export const MANAGING_EDITOR_ROLE = "managing-editor";
+
+//Post Types
+export const ARTICLE_TYPE_BASIC = "basic article";
+export const ARTICLE_TYPE_VIDEO = "video article";
+
+// Post Status
+export const POST_STATUS_DRAFT = "draft";
+export const POST_STATUS_PUBLISHED = "published";

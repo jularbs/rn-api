a2ba707f48d2361f99da8dad80f7cc346ebf5cc5
Ralph Jularbal <ralphjularbal@Ralphs-MacBook-Pro-2.local>
2025-11-02 17:04:07 +0800

    chore: use role constants

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,27 +1,35 @@
 import express from "express";
 import { authenticate, authorize } from "@/middleware";
-import { getAllUsers, getUserById, createUser, updateUser, deleteUser, restoreUser } from "@/controllers/usersController";
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  restoreUser,
+} from "@/controllers/usersController";
+import { ADMIN_ROLE, MANAGER_ROLE, MANAGING_EDITOR_ROLE } from "@/utils/constants";
 
 const router = express.Router();
 
 // BASIC CRUD OPERATIONS
 // POST /v1/users - Register new user
-router.post("/v1/users", authenticate, authorize("admin"), createUser);
+router.post("/v1/users", authenticate, authorize(ADMIN_ROLE, MANAGER_ROLE, MANAGING_EDITOR_ROLE), createUser);
 
 // GET /v1/users - Get all users with pagination and filtering
-router.get("/v1/users", authenticate, authorize("admin"), getAllUsers);
+router.get("/v1/users", authenticate, authorize(ADMIN_ROLE, MANAGER_ROLE, MANAGING_EDITOR_ROLE), getAllUsers);
 
 // GET /v1/users/:id - Get user by ID (Owner or Admin)
-router.get("/v1/users/:id", authenticate, authorize("admin"), getUserById);
+router.get("/v1/users/:id", authenticate, authorize(ADMIN_ROLE, MANAGER_ROLE, MANAGING_EDITOR_ROLE, "owner"), getUserById);
 
 // PUT /v1/users/:id - Update user (Owner or Admin)
-router.put("/v1/users/:id", authenticate, authorize("admin", "owner"), updateUser);
+router.put("/v1/users/:id", authenticate, authorize(ADMIN_ROLE, MANAGER_ROLE, MANAGING_EDITOR_ROLE, "owner"), updateUser);
 
 // DELETE /v1/users/:id - Delete user (soft delete) (Admin only)
-router.delete("/v1/users/:id", authenticate, authorize("admin"), deleteUser);
+router.delete("/v1/users/:id", authenticate, authorize(ADMIN_ROLE), deleteUser);
 
 // SPECIAL RESTful OPERATIONS
 // POST /v1/users/:id/restore - Restore a soft deleted user (Admin only)
-router.post("/v1/users/:id/restore", authenticate, authorize("admin"), restoreUser);
+router.post("/v1/users/:id/restore", authenticate, authorize(ADMIN_ROLE), restoreUser);
 
 export default router;
